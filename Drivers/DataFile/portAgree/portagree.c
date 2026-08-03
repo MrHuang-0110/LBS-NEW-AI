bf25e62bcacad9faddb6ee7e94f448196b59974b
Mr-Huang0110 <2416012532@qq.com>
2026-08-03 14:48:44 +0800

    feat(camer): dynamic N configs in monitor JSON for label mode

--- Drivers/DataFile/portAgree/portagree.c
+++ Drivers/DataFile/portAgree/portagree.c
@@ -821,10 +821,27 @@ void newAiMonitor(void) {
 
 								switch((uint8_t)dev_camer->mode)
 								{
+									case CAMER_LABE_TYPE:
+									{
+										/* 动态 N 组:按本帧目标数输出,不补齐 */
+										int n = (int)dev_camer->n_targets;
+										for(int det = 0; det < n; det++)
+										{
+											int base = det * 10;
+											p = json_objOpen(p, NULL, &remLen);
+											p = json_int(p, "id", dev_camer->data[base], &remLen);
+											p = json_int(p, "x", dev_camer->data[base+1]<<8|dev_camer->data[base+2], &remLen);
+											p = json_int(p, "y", dev_camer->data[base+3]<<8|dev_camer->data[base+4], &remLen);
+											p = json_int(p, "w", dev_camer->data[base+5]<<8|dev_camer->data[base+6], &remLen);
+											p = json_int(p, "h", dev_camer->data[base+7]<<8|dev_camer->data[base+8], &remLen);
+											p = json_int(p, "conf", dev_camer->data[base+9], &remLen);
+											p = json_objClose(p, &remLen);
+										}
+										break;
+									}
 									case CAMER_MENU_TYPE:
 									case CAMER_MODE_TYPE:
 									case CAMER_FACE_TYPE:
-									case CAMER_LABE_TYPE:
 									case CAMER_OBJECT_TYPE:
 									case CAMER_COLOR_TYPE:
 									case CAMER_WAY_TYPE:
@@ -833,6 +850,7 @@ void newAiMonitor(void) {
 									case CAMER_OBJECT_BODY_TYPE:
 									case CAMER_PHOTO_TYPE:
 									{
+										/* 原有固定 4 组,不动 */
 										const char *id_names[4] = {"id1", "id2", "id3", "id4"};
 										for(int det = 0; det < 4; det++)
 										{
