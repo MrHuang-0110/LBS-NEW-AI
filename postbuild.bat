f848f3625272c3b647db75af33293c110bac25ce
Mr-Huang0110 <2416012532@qq.com>
2026-08-14 11:22:31 +0800

    chore(build): switch Keil flash driver to UL2CM3, update postbuild copy path

--- postbuild.bat
+++ postbuild.bat
@@ -1,2 +1,2 @@
 @echo off
-copy "E:\LBS-NEW-AI\MDK-ARM\STM32H723\pikaNewAi.bin" "E:\LBS-Project\NEW-AI-PROJECT\piKaNewAI-bin-downLoad\DownloadFWLIB\LB_FWLIB\app"
+copy "E:\LBS-NEW-AI\MDK-ARM\STM32H723\pikaNewAi.bin" "E:\LBS-FramWare\products\NEW-AI\fwlib\app"
